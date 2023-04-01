05b065395ec14ed0666183acb0851cae8deb6221
s-kvng <kvngnathan8420@gmail.com>
2023-04-01 17:06:21 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ReactDOM  from 'react-dom/client';
+import List from './components/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+    return (
+
+        <div className='container'>
+            <nav className='navbar sticky-top navbar-light bg-black'>
+                <h1 className='navbar-brand text-white'>Ricky and Morty</h1>
+            </nav>
+            <List />
+        </div>
+    );
+}
+
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
